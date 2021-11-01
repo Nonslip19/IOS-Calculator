a5942662f894ab97a139344e1245f24e75d63eec
Mehmet <mkaymaz93@gmail.com>
2021-11-01 11:41:08 +0300

    m

---
+++ app.js
@@ -0,0 +1,23 @@
+var numberBtn = document.querySelectorAll("div");
+console.log(numberBtn);
+var operatorBtn = document.getElementsByClassName("operator");
+var clearBtn = document.getElementsByClassName("clear");
+var plusMinusBtn = document.getElementsByClassName("plus-minus");
+var backBtn = document.getElementsByClassName("back");
+var equalBtn = document.getElementsByClassName("equals");
+var commaBtn = document.getElementsByClassName("comma");
+
+var screenTopBtn = document.getElementsByClassName("screen_top");
+var screenBotBtn = document.getElementsByClassName("screen_bottom");
+
+ 
+
+
+
+
+
+
+
+
+
+
